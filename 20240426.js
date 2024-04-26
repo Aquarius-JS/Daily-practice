efd614ad345c0bfb3dd23976b6f45d1f96dad79c
aquarius <1015772292@qq.com>
2024-04-26 12:02:40 +0800

    .

---
+++ 20240426.js
@@ -0,0 +1,14 @@
+/**
+ * promise
+ */
+
+new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 1000);
+}).then(() => {
+  console.log("1");
+});
+setTimeout(() => {
+  console.log("@");
+}, 1000);

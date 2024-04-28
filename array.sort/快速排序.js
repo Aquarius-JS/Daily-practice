03abbd32508a9171e05d473c96aa7d8c9e4e667a
aquarius <1015772292@qq.com>
2024-04-28 11:15:20 +0800

    双指针实现快排

--- array.sort/快速排序.js
+++ array.sort/快速排序.js
@@ -1,3 +1,8 @@
+/**
+ * 快速排序
+ * @param {Array} arr
+ * @returns {Array}
+ */
 function sort(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -31,3 +36,35 @@ function sort(arr) {
   return arr;
 }
 console.log(sort([4, 1, 5, 3, 5, -1, 4, 2, 2, 10, -4, 3]));
+
+/**
+ * 双指针实现 temp p
+ * @param {Array} arr 
+ * @returns 
+ */
+function sort_two(arr) {
+  if (arr.length <= 1) return arr;
+  let temp = 0;
+  let p = arr.length - 1;
+  while (p !== temp) {
+    while (p > temp) {
+      if (arr[p] < arr[temp]) {
+        [arr[p], arr[temp]] = [arr[temp], arr[p]];
+        [p, temp] = [temp + 1, p];
+        break;
+      }
+      p--;
+    }
+    while (p < temp) {
+      if (arr[p] > arr[temp]) {
+        [arr[p], arr[temp]] = [arr[temp], arr[p]];
+        [p, temp] = [temp - 1, p];
+        break;
+      }
+      p++;
+    }
+  }
+  return [...sort_two(arr.slice(0, temp)), arr[temp], ...sort_two(arr.slice(temp + 1))];
+}
+
+console.log(sort_two([4, 1, 5, 3, 5, -1, 4, 2, 2, 10, -4, 3]));

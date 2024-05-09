809f3c43a61d0b8b6e0809de71f1068f60dba176
aquarius <1015772292@qq.com>
2024-05-09 12:06:50 +0800

    myasync

---
+++ es6/generator.js
@@ -0,0 +1,56 @@
+/**
+ * 运行Generator函数的函数实现运行async函数
+ * @param {Iterator} iter generator函数执行后的 Iterator
+ * @param {*} value
+ */
+function RunAsync(iter, value) {
+  let _res = iter.next(value);
+  return new Promise(resolve => {
+    const handleResult = _res => {
+      if (_res.done) {
+        resolve(_res.value);
+      } else {
+        Promise.resolve(_res.value).then(
+          val => {
+            resolve(RunAsync(iter, val));
+          },
+          err => {
+            try {
+              // 尝试将错误传递回Generator函数
+              handleResult(iter.throw(err));
+            } catch (error) {
+              // 如果Generator函数内部没有捕获错误，则reject
+              reject(error);
+            }
+          }
+        );
+      }
+    };
+    handleResult(_res);
+  });
+}
+
+/**
+ * 模拟一个async函数 yield <-> await
+ * @returns {Promise}
+ */
+function* myAsync() {
+  let res = yield Promise.resolve('1');
+  console.log(res, 'success');
+  res = yield new Promise(resolve => {
+    setTimeout(() => {
+      console.log('test');
+      resolve(2);
+    }, 3000);
+  });
+  console.log(res);
+  try {
+    yield Promise.reject('err');
+  } catch (err) {
+    console.log('12');
+  }
+  return 3;
+}
+
+RunAsync(myAsync()).then(console.log);
+//1 success test 2 12 3

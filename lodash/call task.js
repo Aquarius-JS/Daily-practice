85be7bd7ace27d94369d8c3aa182ff0db320d7aa
aquarius <1015772292@qq.com>
2024-11-03 20:02:33 +0800

    .

---
+++ lodash/call task.js
@@ -0,0 +1,21 @@
+async function cb() {
+    await new Promise((res) => {
+        setTimeout(() => {
+            res(1)
+        }, 3000)
+    })
+    console.log(1)
+    cb()
+    // return 2
+}
+const res = await cb()
+console.log(res, "!!!")
+
+async function f(num) {
+    if (num <= 1) {
+        return 1;
+    }
+    return f(num - 1) + f(num - 2)
+}
+
+f(6).then(console.log)
